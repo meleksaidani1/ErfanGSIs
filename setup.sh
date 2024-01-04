d082efa03a2efe471df333022157e17bbfe634cf
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-04 18:03:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ -n "$(command -v pkg)" ]]; then
-        pkg update && pkg upgrade pkg install proot-distro  pkg install p7zip pkg install aria2 pkg install wget pkg install python git coreutils pkg install -y unace unrar zip unzip p7zip p7zip-plugins sharutils uudeview arj file dtc python brotli lz4 gawk aria2 pkg install tsu pkg search p7zip
+        pkg update && pkg upgrade pkg install proot-distro  pkg install p7zip pkg install aria2 pkg install wget pkg install python git coreutils  
 
         # Adjust installation commands based on what's available in Termux packages
     else
