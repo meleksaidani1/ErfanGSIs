a6d616ececc2c191e724e8b7fdcbb3efc6b0633f
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-04 12:04:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ -n "$(command -v pkg)" ]]; then
-        pkg update && pkg upgrade pkg install python git coreutils pkg install -y unace unrar zip unzip p7zip p7zip-plugins sharutils uudeview arj file dtc python brotli lz4 gawk aria2 pkg install sudo
+        pkg update && pkg upgrade pkg install python git coreutils pkg install -y unace unrar zip unzip p7zip p7zip-plugins sharutils uudeview arj file dtc python brotli lz4 gawk aria2 pkg install tsu
         # Adjust installation commands based on what's available in Termux packages
     else
         echo "Please install 'pkg' package manager in Termux."
