1eed7da9554887069c5cb66fadaf6d37672a91c9
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-04 11:00:12 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+LOCALDIR=$(dirname "$(realpath "$0")")
 TOOLS_DIR="$LOCALDIR/tools"
 
+# Ensure git is installed in Termux
+pkg install git
+
+# Move to the directory containing tools
+cd "$TOOLS_DIR" || exit
+
+# Initialize and update submodules
 git submodule update --init --recursive
 git pull --recurse-submodules
 
 if [[ -d "$TOOLS_DIR/Firmware_extractor" ]]; then
-    git -C "$TOOLS_DIR"/Firmware_extractor fetch origin
-    git -C "$TOOLS_DIR"/Firmware_extractor reset --hard origin/master
+    # Fetch the latest changes and reset to origin/master
+    git -C "$TOOLS_DIR/Firmware_extractor" fetch origin
+    git -C "$TOOLS_DIR/Firmware_extractor" reset --hard origin/master
 else
-    git clone -q https://github.com/erfanoabdi/Firmware_extractor "$TOOLS_DIR"/Firmware_extractor
+    # Clone the repository if it doesn't exist
+    git clone -q https://github.com/meleksaidani1/Firmware_extractor "$TOOLS_DIR"/Firmware_extractor
 fi
