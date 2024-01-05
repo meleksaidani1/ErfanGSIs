a0ed5f0482c7b43ad94ec85dd0a83c3ff21dc1d8
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 18:56:40 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+alias umount="busybox umount"
+
 # ... [rest of your script remains unchanged]
 
 echo "Create Temp dir"
-tempdir="/data/data/com.termux/files/home/temp"  # Replace this with your desired temporary directory path in Termux
+tempdir="/path/to/temp_directory"  # Replace this with your desired temporary directory path
 rm -rf "$tempdir"
-mkdir -p "$tempdir"
+mkdir -p "$systemdir"
 
 if [ "$sourcetype" == "Aonly" ]; then
     echo "Warning: Aonly source detected, using P AOSP ramdisk"
     cd "$systemdir"
-    tar xf "$prebuiltdir/ABrootDir.tar"
+    fakeroot tar xf "$prebuiltdir/ABrootDir.tar"
     cd "$LOCALDIR"
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; tar cf - . ) | ( cd "$systemdir/system" ; tar xf - )
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir/system" ; fakeroot tar xf - )
     cd "$LOCALDIR"
-    # Modify the following lines to edit build.prop without using sed or modify permissions if possible in Termux
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
     echo "ro.build.system_root_image=false" >> "$systemdir/system/build.prop"
 else
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; tar cf - . ) | ( cd "$systemdir" ; tar xf - )
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir" ; fakeroot tar xf - )
     if [[ -e "$sourcepath/mounted.txt" ]]; then
         for p in $(cat "$sourcepath/mounted.txt"); do
             [[ $p = system ]] && continue
             [[ $p = vendor ]] && continue
             if [[ -L "$systemdir/system/$p" ]]; then
-                rm -rf "$systemdir/system/$p"
+                fakeroot rm -rf "$systemdir/system/$p"
                 mkdir "$systemdir/system/$p"
-                rm -rf "$systemdir/$p"
+                fakeroot rm -rf "$systemdir/$p"
                 ln -s "/system/$p" "$systemdir/$p"
-                ( cd "$sourcepath/$p" ; tar cf - . ) | ( cd "$systemdir/system/$p" ; tar xf - )
+                ( cd "$sourcepath/$p" ; fakeroot tar cf - . ) | ( cd "$systemdir/system/$p" ; fakeroot tar xf - )
             fi
         done
     fi
     cd "$LOCALDIR"
-    # Modify the following lines to edit build.prop without using sed or modify permissions if possible in Termux
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
     echo "ro.build.system_root_image=true" >> "$systemdir/system/build.prop"
 fi
 
