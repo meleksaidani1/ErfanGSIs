b88760e4b183484ae3e62d89c68daa30ac96027a
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 18:29:11 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias umount="busybox umount"
+
 # ... [rest of your script remains unchanged]
 
 echo "Create Temp dir"
@@ -9,17 +11,30 @@ mkdir -p "$systemdir"
 if [ "$sourcetype" == "Aonly" ]; then
     echo "Warning: Aonly source detected, using P AOSP ramdisk"
     cd "$systemdir"
-    tar xf "$prebuiltdir/ABrootDir.tar"
+    fakeroot tar xf "$prebuiltdir/ABrootDir.tar"
     cd "$LOCALDIR"
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; tar cf - . ) | ( cd "$systemdir/system" ; tar xf - )
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir/system" ; fakeroot tar xf - )
     cd "$LOCALDIR"
     sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
     sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
     echo "ro.build.system_root_image=false" >> "$systemdir/system/build.prop"
 else
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; tar cf - . ) | ( cd "$systemdir" ; tar xf - )
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir" ; fakeroot tar xf - )
+    if [[ -e "$sourcepath/mounted.txt" ]]; then
+        for p in $(cat "$sourcepath/mounted.txt"); do
+            [[ $p = system ]] && continue
+            [[ $p = vendor ]] && continue
+            if [[ -L "$systemdir/system/$p" ]]; then
+                fakeroot rm -rf "$systemdir/system/$p"
+                mkdir "$systemdir/system/$p"
+                fakeroot rm -rf "$systemdir/$p"
+                ln -s "/system/$p" "$systemdir/$p"
+                ( cd "$sourcepath/$p" ; fakeroot tar cf - . ) | ( cd "$systemdir/system/$p" ; fakeroot tar xf - )
+            fi
+        done
+    fi
     cd "$LOCALDIR"
     sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
     sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
