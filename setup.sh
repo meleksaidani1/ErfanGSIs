2ff70e4e577574cc7bc113c78c58a53c7d12a3f5
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 16:21:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ -n "$(command -v pkg)" ]]; then
-        pkg update && pkg upgrade pkg install proot-distro  pkg install p7zip pkg install aria2 pkg install wget pkg install python git coreutils  
 
         # Adjust installation commands based on what's available in Termux packages
     else
