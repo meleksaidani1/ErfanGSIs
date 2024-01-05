3c8e4f876fec286d4b5c15b870b61174768597e3
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 17:56:50 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias umount="busybox umount"
+
 # ... [rest of your script remains unchanged]
 # Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
 
@@ -29,24 +31,24 @@ if [ "$sourcetype" == "Aonly" ]; then
     tar xf "$prebuiltdir/ABrootDir.tar"
     cd "$LOCALDIR"
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; sudo tar cf - . ) | ( cd "$systemdir/system" ; sudo tar xf - )
+    ( cd "$systempath" ; tsudo tar cf - . ) | ( cd "$systemdir/system" ; tsudo tar xf - )
     cd "$LOCALDIR"
     sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
     sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
     echo "ro.build.system_root_image=false" >> "$systemdir/system/build.prop"
 else
     echo "Making copy of source rom to temp"
-    ( cd "$systempath" ; sudo tar cf - . ) | ( cd "$systemdir" ; sudo tar xf - )
+    ( cd "$systempath" ; tsudo tar cf - . ) | ( cd "$systemdir" ; tsudo tar xf - )
     if [[ -e "$sourcepath/mounted.txt" ]]; then
         for p in $(cat "$sourcepath/mounted.txt"); do
             [[ $p = system ]] && continue
             [[ $p = vendor ]] && continue
             if [[ -L "$systemdir/system/$p" ]]; then
-                sudo rm -rf "$systemdir/system/$p"
+                tsudo rm -rf "$systemdir/system/$p"
                 mkdir "$systemdir/system/$p"
-                sudo rm -rf "$systemdir/$p"
+                tsudo rm -rf "$systemdir/$p"
                 ln -s "/system/$p" "$systemdir/$p"
-                ( cd "$sourcepath/$p" ; sudo tar cf - . ) | ( cd "$systemdir/system/$p" ; sudo tar xf - )
+                ( cd "$sourcepath/$p" ; tsudo tar cf - . ) | ( cd "$systemdir/system/$p" ; tsudo tar xf - )
             fi
         done
     fi
@@ -62,5 +64,5 @@ echo "Patching started..."
 # ... [rest of your script remains unchanged]
 
 echo "Remove Temp dir"
-sudo umount "$tempdir"
+tsudo umount "$tempdir"
 rm -rf "$tempdir"
