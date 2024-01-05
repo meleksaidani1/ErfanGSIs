b8be15f9cc176de74962994d95c3e4d256e9ad61
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 20:47:48 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,38 +7,38 @@ tempdir="/path/to/temp_directory"  # Replace this with your desired temporary di
 rm -rf "$tempdir"
 mkdir -p "$systemdir"
 
-# Function to perform actions within the chroot environment
-perform_chroot_actions() {
-    chroot "$systemdir" /bin/bash -c '
-        # Perform actions within the chroot environment here
-        # This could include setting up the environment or manipulating files
-        
-        if [ "$1" == "Aonly" ]; then
-            echo "Warning: Aonly source detected, using P AOSP ramdisk"
-            cd /path/to/AOSP_ramdisk_directory  # Replace with the actual path to AOSP ramdisk
-            tar xf "$prebuiltdir/ABrootDir.tar"
-            cd /path/to/LOCALDIR
-            echo "Making copy of source rom to temp"
-            ( cd /path/to/systempath ; tar cf - . ) | ( cd /system ; tar xf - )
-            cd /path/to/LOCALDIR
-            sed -i "/ro.build.system_root_image/d" /system/build.prop
-            sed -i "/ro.build.ab_update/d" /system/build.prop
-            echo "ro.build.system_root_image=false" >> /system/build.prop
-        else
-            echo "Making copy of source rom to temp"
-            ( cd /path/to/systempath ; tar cf - . ) | ( cd /system ; tar xf - )
-            
-            # Other actions for non-Aonly source
-            
-        fi
-        
-        # Other operations within the chroot environment
-        # ...
-    '
-}
-
-# Execute chroot actions
-perform_chroot_actions "$sourcetype"
+if [ "$sourcetype" == "Aonly" ]; then
+    echo "Warning: Aonly source detected, using P AOSP ramdisk"
+    cd "$systemdir"
+    fakeroot tar xf "$prebuiltdir/ABrootDir.tar"
+    cd "$LOCALDIR"
+    echo "Making copy of source rom to temp"
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir/system" ; fakeroot tar xf - )
+    cd "$LOCALDIR"
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
+    echo "ro.build.system_root_image=false" >> "$systemdir/system/build.prop"
+else
+    echo "Making copy of source rom to temp"
+    ( cd "$systempath" ; fakeroot tar cf - . ) | ( cd "$systemdir" ; fakeroot tar xf - )
+    if [[ -e "$sourcepath/mounted.txt" ]]; then
+        for p in $(cat "$sourcepath/mounted.txt"); do
+            [[ $p = system ]] && continue
+            [[ $p = vendor ]] && continue
+            if [[ -L "$systemdir/system/$p" ]]; then
+                fakeroot rm -rf "$systemdir/system/$p"
+                mkdir "$systemdir/system/$p"
+                fakeroot rm -rf "$systemdir/$p"
+                ln -s "/system/$p" "$systemdir/$p"
+                ( cd "$sourcepath/$p" ; fakeroot tar cf - . ) | ( cd "$systemdir/system/$p" ; fakeroot tar xf - )
+            fi
+        done
+    fi
+    cd "$LOCALDIR"
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
+    echo "ro.build.system_root_image=true" >> "$systemdir/system/build.prop"
+fi
 
 # ... [rest of your script remains unchanged]
 
